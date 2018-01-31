f542a40d139534f5aa3ae950365d72fe1a6688e9
Kristin Dragos <KristinDragos@gmail.com>
2018-01-31 15:48:59 -0800

    Start on basic movement logic

---
+++ Assets/Rocket.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();
+	}
+
+    void ProcessInput()
+    {
+        if(Input.GetKey(KeyCode.Space)) {
+            Debug.Log("Thrust.");
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            Debug.Log("Rotate left");
+        } else if (Input.GetKey(KeyCode.D)) {
+            Debug.Log("Rotate right.");
+        }
+    }
+}
